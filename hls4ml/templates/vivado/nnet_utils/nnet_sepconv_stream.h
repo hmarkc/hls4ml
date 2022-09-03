56b475ad648684f1db22fd002edeac1cbf9d9053
hmarkc <a77069752@gmail.com>
2022-09-03 11:36:26 +0100

    does not partition sepconv stream data completely

--- hls4ml/templates/vivado/nnet_utils/nnet_sepconv_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_sepconv_stream.h
@@ -144,7 +144,7 @@ void pointwise_mult_buffer(
     #pragma HLS INLINE
 
     typename data_T::value_type data[CONFIG_T::n_chan];
-    #pragma HLS ARRAY_PARTITION variable=data complete
+    // #pragma HLS ARRAY_PARTITION variable=data complete
 
     typename res_T::value_type res[CONFIG_T::n_filt];
     #pragma HLS ARRAY_PARTITION variable=res complete
