37a66edcabc9cc88f94451d9769408e23661d1fa
hmarkc <a77069752@gmail.com>
2022-09-15 12:05:57 +0100

    Revert "random matrix"
    This reverts commit b86e7f43b929016b6dbf8f51b55971f6d2378a6a.

--- hls4ml/templates/vivado/nnet_utils/nnet_dropout_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_dropout_stream.h
@@ -20,52 +20,43 @@
 #ifndef NNET_DROPOUT_STREAM_H_
 #define NNET_DROPOUT_STREAM_H_
 
+#include <cmath>
 #include "ap_fixed.h"
 #include "hls_stream.h"
 #include "nnet_common.h"
-#include "nnet_dropout.h"
-#include "nnet_stream.h"
 #include "nnet_types.h"
-#include <cmath>
-
+#include "nnet_stream.h"
+#include "nnet_dropout.h"
 
 namespace nnet {
 
 // *************************************************
 //       Bayesian Dropout
 // *************************************************
-template <class data_T, class res_T, typename CONFIG_T>
+template<class data_T, class res_T, typename CONFIG_T>
 void dropout(hls::stream<data_T> &data, hls::stream<res_T> &res, int seed) {
 
   static std::minstd_rand generator(seed);
   float keep_rate = 1 - CONFIG_T::drop_rate;
   float max = generator.max();
-  float rand_matrix[CONFIG_T::n_in];
-RandomNumLoop:
-  for (int i = 0; i < CONFIG_T::n_in; i++) {
-#pragma HLS UNROLL
-    rand_matrix[i] = (float)generator() / max;
-  }
+    DropoutLoop: for (int i = 0; i < CONFIG_T::n_in / res_T::size; i++) {
+        #pragma HLS pipeline
 
-DropoutLoop:
-  for (int i = 0; i < CONFIG_T::n_in / res_T::size; i++) {
-#pragma HLS pipeline
+        data_T in_data = data.read();
+        res_T out_data;
+        #pragma HLS DATA_PACK variable=out_data
 
-    data_T in_data = data.read();
-    res_T out_data;
-    //        #pragma HLS DATA_PACK variable=out_data
-
-  DropoutPackLoop:
-    for (int j = 0; j < res_T::size; j++) {
-#pragma HLS UNROLL
-      typename data_T::value_type zero = {};
-      typename data_T::value_type temp =
-          rand_matrix[j] < keep_rate ? in_data[j] : zero;
-      out_data[j] = temp * (typename data_T::value_type)keep_rate;
+        DropoutPackLoop: for (int j = 0; j < res_T::size; j++) {
+            #pragma HLS UNROLL
+            typename data_T::value_type zero = {};
+            typename data_T::value_type temp =
+                ((float)generator() / max) < keep_rate
+                    ? in_data[j] : zero;
+            out_data[j] = temp * (typename data_T::value_type)keep_rate;
+        }
+        res.write(out_data);
     }
-    res.write(out_data);
-  }
 }
-} // namespace nnet
+}
 
 #endif
